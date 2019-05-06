cfd65232de1b359dec1ff2d674f34e97b48bb390
freeWillee <will.ik.lee@gmail.com>
2019-05-06 16:26:52 -0600

    make controls secions

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from '*.module.css';
+import classes from './BuildControl.module.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
