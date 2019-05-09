16b5b49c2c606276bf3b4be35323d432fa3411fc
freeWillee <will.ik.lee@gmail.com>
2019-05-09 15:11:31 -0600

    Use axios - hookup firebase backend; send post request to checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -91,7 +92,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continued the shopping process!')
+        // alert('You continued the shopping process!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Sebby',
+                address: {
+                    street: 'Main St',
+                    postalCode: 'h0h0h0',
+                    country: 'Canada'
+                },
+                email: 'sebby@mail.com'
+            },
+            deliveryMethod: '24 HRS'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error=> console.log(error));
     }
 
     render() {
